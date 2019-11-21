c91aa41835a06dfeb05c0869f2591bbe6db69fcf
yzj <641854024@example.com>
2019-11-21 14:51:11 +0800

    update solution name

---
+++ Core_WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Core_Entity.Dto;
+using Infrastructure;
+using Infrastructure.AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace yzj.Controllers
+{
+    //允许的response类型
+    [Produces(MediaTypeNames.Application.Json)]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly BasicDbContext _context;
+        public HomeController(ILogger<HomeController> logger, BasicDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public string Index()
+        {
+            string groupName = string.Empty;
+
+            var group = _context.Groups.FirstOrDefault();
+            Dto_Group dtoGroup = group.MapTo<Dto_Group>();
+            groupName = group.Name;
+
+            _logger.LogInformation("Hello, this is the index!");
+            return dtoGroup.GroupName;
+        }
+    }
+}
---
+++ Core_WebAPI/Startup.cs
@@ -0,0 +1,118 @@
+using AutoMapper;
+using Infrastructure;
+using Infrastructure.AutoMapper;
+using Infrastructure.Server;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+
+//将所有controller定义为webapi，且无法针对某个控制器退出webapi的定义
+//[assembly:ApiController]
+namespace yzj
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            var basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);//应用程序所在目录
+            string xmlFile = Assembly.GetExecutingAssembly().GetName().Name + ".xml";
+            //项目生成的xml文档
+            string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            services.AddControllers();
+            //注入DBContext
+            services.AddDbContextToService<BasicDbContext>(DataBaseTypeEnum.SqlServer, Configuration.GetConnectionString("SqlConnection"));
+            #region 通过dll注入服务
+            //services.AddDataService();
+            #endregion
+            #region json.net注入
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                    options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc; // 设置时区为 UTC
+                    options.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+                });
+            #endregion
+            #region swagger文档注入
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "MyApi",
+                    Version = "v1",
+                    Description = "A simple example ASP.NET Core Web API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = string.Empty,
+                        Email = string.Empty,
+                        Url = new Uri("http://www.baidu.com")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Nothing",
+                        Url = new Uri("https://example.com/license")
+                    }
+                }); ;
+                //具有冲突（重复命名方法）取第一个
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                c.IncludeXmlComments(xmlPath);
+            });
+            #endregion
+            #region 自动注入Mapper映射规则
+            services.AddAutoMapper(MappingRegister.MapTypes());
+            #endregion
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            var httpContextAccessor = app.ApplicationServices.GetRequiredService<Microsoft.AspNetCore.Http.IHttpContextAccessor>();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseStateAutoMapper();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                     name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                //endpoints.MapControllerRoute(name: "areaRoute", "{area:exists}/{controller=Home}/{action=Index}"); // 区域路由
+            });
+        }
+    }
+}
